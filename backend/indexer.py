e655983ef73460472fab37028ccde6ecc67298cf
bharat5007 <bharatgoyal5007@gmail.com>
2026-06-19 21:47:41 +0530

    basic indexer added

--- backend/indexer.py
+++ backend/indexer.py
@@ -1,26 +1,54 @@
 import tree_sitter_python as tspython
-from tree_sitter import Language, Parser
+from tree_sitter import Language, Parser, Node
 
+INTERESTING_TYPES = [
+    "class_definition",
+    "function_definition",
+    "import_statement",
+    "import_from_statement",
+]
 PY_LANGUAGE = Language(tspython.language())
 parser = Parser(PY_LANGUAGE)
 
-content = open("rough2.py", "r").read().encode("utf-8")
+content = open("rough4.py", "r").read().encode("utf-8")
 
 tree = parser.parse(content)
 root_node = tree.root_node
 
-x = root_node.children
-# print(root_node.children[1].text)
-for i in x:
-    print(f"{i.text}     {i.type}      {len(i.children)}")
-    print()
-    if i.type == "function_definition":
-        for child in i.children:
-            print(child.type, "-->", child.text)
+data = root_node.children
+chunks = []
 
-    print()
-    print()
 
-# print("Root node type:", root_node.children)
-print()
-# print(f"<<<<<<<<<<<< {root_node}")  # poora syntax tree S-expression form mein
+def prepare_metadata(x: Node):
+    meta_data = {
+        "type": x.type,
+        "file": "rough2.py",
+        "start_line": x.start_point,
+        "end_line": x.end_point,
+        "source": x.text,
+    }
+    return meta_data
+
+
+def prepare_chunks(data: Node):
+
+    metadata = None
+    if data.type in INTERESTING_TYPES:
+        metadata = prepare_metadata(data)
+
+    name_node = data.child_by_field_name("name")
+    if name_node:
+        metadata["name"] = name_node.text
+
+    for child in data.children:
+        if child.is_named:
+            prepare_chunks(child)
+
+    if metadata:
+        chunks.append(metadata)
+
+
+for x in data:
+    prepare_chunks(x)
+
+print(chunks)
